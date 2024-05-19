c7fce8b30cb8d7480c5d329ae28a80a2bf3a1de8
greggsman <elliotdg10@icloud.com>
2024-05-19 16:01:16 +0100

    started implementing Djikstra's shortest path starts infinitely looping at C, idk why

--- Djikstra/Djikstra/Program.cs
+++ Djikstra/Djikstra/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection.Metadata;
+﻿using System.Dynamic;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 
 class UnDirectedWeightedStringGraph 
 {
@@ -36,49 +39,116 @@ class UnDirectedWeightedStringGraph
         AddColumnToEdges();
     }
     public int GetConnection(int i, int j) { return adjacencyMatrix[i][j]; }
-    public virtual void SetConnection(int i, int j, int value) 
-    { 
+    public virtual void SetConnection(string node1, string node2, int value) 
+    {
+        int i = GetNodeIndex(node1);
+        int j = GetNodeIndex(node2);
         adjacencyMatrix[i][j] = value;
         adjacencyMatrix[j][i] = value;
     }
     public void GetConnections()
     {
+        Console.Write(" ");
+        foreach(string node in nodes) Console.Write(node);
+        Console.WriteLine();
         for(int i = 0; i < adjacencyMatrix.Count; i++)
         {
+            Console.Write(nodes[i]);
             for(int j = 0; j < nodes.Count; j++)
             {
-                Console.Write(GetConnection(i,j) + " ");
+                int connectionFound = GetConnection(i, j);
+                Console.Write(connectionFound);
             }
             Console.WriteLine();
         }
     }
-    /*
-    public string FindShortestPath(string start, string end){
-        int startIndex = -1;
-        int endIndex = -1;
-        for(int i = 0; i < nodes.Count; i++){
+    bool[] visited;
+    int[] distanceFromStart;
+    string[] previousVertex;
+    public void FindShortestPath(string start, string destination){
+        int nodesCount = nodes.Count;
+        visited = new bool[nodesCount];
+        distanceFromStart = new int[nodesCount];
+        previousVertex = new string[nodesCount];
+        string emptyPreviousVertex = "";
+
+        // populate arrays
+        for(int i = 0; i < nodesCount; i++){
+            visited[i] = false;
             if(nodes[i] == start){
-                startIndex = i;
+                distanceFromStart[i] = 0;
             }
-            if(nodes[i] == end){
-                startIndex = i;
+            else{
+                distanceFromStart[i] = int.MaxValue;
+            }
+            previousVertex[i] = emptyPreviousVertex;
+        }
+        
+        // Begin traversing
+        int destinationIndex = GetNodeIndex(destination);
+        string nodeToVisit = start;
+        while(!visited[destinationIndex]){
+            Visit(nodeToVisit);
+            int shortestDistanceIndex = 0;
+            int currentSmallestDistance = int.MaxValue;
+            for(int i = 0; i < nodes.Count; i++){
+                if(distanceFromStart[i] > 0){
+                    if(distanceFromStart[i] < currentSmallestDistance){                       
+                        currentSmallestDistance = distanceFromStart[i];
+                        shortestDistanceIndex = i;
+                    }
+                }
+            }
+            nodeToVisit = nodes[shortestDistanceIndex];
+        }
+    }
+    private void Visit(string node){
+        Console.WriteLine("visiting " + node);
+        int nodeIndex = GetNodeIndex(node);
+        for(int i = 0; i < nodes.Count; i++){
+            int connection = GetConnection(i, nodeIndex);
+            if(!visited[i]){
+                if(connection != defaultValue){ // check for connection
+                    int newDistanceFromStart = distanceFromStart[nodeIndex] + connection;
+                    if(newDistanceFromStart < distanceFromStart[i]){
+                        distanceFromStart[i] = newDistanceFromStart;
+                    }
+                }
             }
         }
-        else{
-            return start;
+        visited[nodeIndex] = true;
+        foreach(int distance in distanceFromStart){
+            if(distance == int.MaxValue){
+                Console.WriteLine("Inf");
+            }
+            else{
+                Console.WriteLine(distance);
+            }
         }
-        int[] connections = new int[nodes.Count];
-        return Djikstras(startIndex, endIndex, connections);
     }
-    private string Djikstras(int startIndex, int endIndex, int[] connections, List<string> path){
-        if(startIndex == endIndex){
-            // base case
+    public int GetNodeIndex(string node){
+        for(int i = 0; i < nodes.Count; i++){
+            if(node == nodes[i]){
+                return i;
+            }
         }
-        for(int i = 0; i < connections.Length; i++){
-            int connectionValue = GetConnection(startIndex, i);
-            connections[i] += connectionValue;
-        }
-        Djikstras(startIndex, endIndex, connections);
+        return -1;
     }
-    */
+}
+class Program{
+    static void Main(string[] args){
+        List<string> nodes = new List<string>() {"A", "B", "C", "D", "E", "F"};
+        UnDirectedWeightedStringGraph graph = new UnDirectedWeightedStringGraph(nodes);
+        graph.SetConnection("A", "B", 9);
+        graph.SetConnection("A", "C", 5);
+        graph.SetConnection("B", "D", 4);
+        graph.SetConnection("C", "D", 8);
+        graph.SetConnection("B", "E", 6);
+        graph.SetConnection("D", "E", 7);
+        graph.SetConnection("D", "F", 4);
+        graph.SetConnection("E", "F", 3);
+        graph.GetConnections();
+
+        graph.FindShortestPath("A", "F");
+    }
 }
--- Djikstra/Djikstra/obj/Debug/net8.0/Djikstra.AssemblyInfo.cs
+++ Djikstra/Djikstra/obj/Debug/net8.0/Djikstra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Djikstra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d557a7a6a12317fe6a4e09908b3dae3c2845c9c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c730fa1b6d4f6986b574298d351ead91009b0ce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Djikstra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Djikstra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
