e07281a25ad8d620b35a6724c33ae5ea39992298
greggsman <elliotdg10@icloud.com>
2024-05-20 17:46:35 +0100

    uhh

--- SearchingAndSorting/SearchingAndSorting/Program.cs
+++ SearchingAndSorting/SearchingAndSorting/Program.cs
@@ -47,4 +47,64 @@
         }
         return false;
     }
+    static int[] mergeSort(int[] a)
+    {
+        //base case
+        if( a.Length <= 1)
+        { return a; }
+
+        //recursive case
+        //split it into two halves
+        int midpoint = a.Length / 2;
+        int[]left= new int[midpoint];
+        int[]right= new int[a.Length - midpoint];
+
+        for (int i = 0; i < midpoint; i++) // we take the vaules up to them midpoint into the left  half
+        {
+            left[i] = a[i];
+        }
+        for(int i=midpoint; i < a.Length; i++) // we want values from the midpoint into the right half
+        {
+            right[i - midpoint] = a[i];
+        
+        }
+        // merge sort each half
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        //recombine
+
+        int leftv = 0;
+        int rightv = 0;
+        int ansPointer = 0;
+
+        while(leftv<left.Length || rightv<right.Length) // are there values left to enter?
+        {
+            if(leftv < left.Length && rightv < right.Length) //are there values on both sides?
+            {
+                if (left[leftv] < right[rightv]) // if the left value is smaller take it
+                { 
+                    a[ansPointer] = left[leftv];
+                    leftv=leftv+1;          
+                }
+                else
+                {
+                    a[ansPointer] = right[rightv]; //take the right value
+                    rightv=rightv+1;
+                }
+            }
+            else if (leftv < left.Length) // only vlaues on the left
+            {
+                a[ansPointer] = left[leftv];
+                leftv = leftv + 1;
+            }
+            else if(rightv<right.Length) // only values on the right
+            {
+                a[ansPointer] = right[rightv];
+                rightv = rightv + 1;
+            }
+            ansPointer = ansPointer + 1;
+        }
+        return a;
+    }
 }
